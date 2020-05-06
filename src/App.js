bf6265232af9600b271b66465a5a1928d2bfa98b
Minsoo <zlemzlem5656@naver.com>
2020-05-06 21:06:50 +0900

    2020/05/06

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "리액트x";
+  return <>{name === "리액트" ? <div>리액트</div> : <div>리액트 x</div>}</>;
 }
 
 export default App;
